386de52a9bd9debb4dacc34ddde5158bcaf019f1
berraemeksiz <146026981+berraemeksiz@users.noreply.github.com>
2024-05-06 22:01:02 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+
+@app.route("/index2")
+def index2():
+    return render_template('index2.html')
+
+
+app.run(debug=True)
